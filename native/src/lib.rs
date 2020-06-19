683fe4cca23ab12ce14607c1c7ab8e1fc5f0e4cc
FlareonTheProgrammer <theinnovator64@gmail.com>
2020-06-18 19:42:33 -0500

    Added RNG function

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,9 +1,28 @@
 use neon::prelude::*;
+use rand::Rng;
+use std::format;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+fn do_act(mut cx: FunctionContext) -> JsResult<JsString> {
+    let acts = ["write about", "draw"];
+    let subjects = ["landscape", "camping trip", "sunset"];
+
+    let rand_subj = format!(
+        "You should {} a {}",
+        acts[rand::thread_rng().gen_range(0, 2)],
+        subjects[rand::thread_rng().gen_range(0, 3)]
+    );
+
+    Ok(cx.string(rand_subj))
+}
+
+fn rn_gen(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let lower = cx.argument::<JsNumber>(0)?.value().round() as i64;
+    let upper = cx.argument::<JsNumber>(1)?.value().round() as i64;
+    Ok(cx.number(rand::thread_rng().gen_range(lower, upper) as f64))
 }
 
 register_module!(mut cx, {
-    cx.export_function("hello", hello)
+    cx.export_function("doAct", do_act)?;
+    cx.export_function("rnGen", rn_gen)?;
+    Ok(())
 });
